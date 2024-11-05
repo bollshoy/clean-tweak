d08bf2eff6a4835fc0c15ed98f5d84e6667f5555
bollshoy <b.orlov7709@gmail.com>
2024-11-05 15:54:45 +0200

    upd rc pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,39 +2,68 @@ import React, {useCallback, useState} from 'react';
 
 import discord from '@/assets/icons/discord.svg';
 import cloud from '@/assets/icons/cloud.svg';
-
-import './About.css'
+import defender from '@/assets/icons/security.svg';
+import upd from '@/assets/icons/update.svg';
+import './About.css';
 
 const About = () => {
+    const [isDefenderChecked, setIsDefenderChecked] = useState(false);
+    const [isUpdateChecked, setIsUpdateChecked] = useState(false);
 
-    const [isChecked, setIsChecked] = useState(false);
-
-    const handleToggle = useCallback(() => {
-        setIsChecked(!isChecked);
+    const handleDefenderToggle = useCallback(() => {
+        setIsDefenderChecked(!isDefenderChecked);
         document.body.classList.toggle('is-active');
-    })
+    }, [isDefenderChecked]);
+
+    const handleUpdateToggle = useCallback(() => {
+        setIsUpdateChecked(!isUpdateChecked);
+        document.body.classList.toggle('is-active');
+    }, [isUpdateChecked]);
 
     return (
         <section className="about">
             <div className="about__container">
                 <div className="about__item item-1">
                     <div className="about__btn">
-                        <span className="about__title">Windows DEFENDER</span>
+                        <div className="about__btn-title">
+                            <img src={defender} alt="defender" className="about__btn-img"/>
+                            <span className="about__title">Windows DEFENDER</span>
+                        </div>
                         <input
                             className="about__switch"
                             type="checkbox"
-                            id="switch__checkbox"
-                            checked={isChecked}
-                            onChange={handleToggle}
+                            id="switch__defender"
+                            checked={isDefenderChecked}
+                            onChange={handleDefenderToggle}
                         />
-                        <label htmlFor="switch__checkbox" className="switch__label">
+                        <label htmlFor="switch__defender" className="switch__label">
                             <span className="switch__circle"></span>
                         </label>
                     </div>
                     <p className="about__text">
-                        Не надо думать о твиках — все просто!<br/> Все нужные функции доступны по одному нажатию мыши.
+                        Не надо думать о твиках — все просто! Все нужные функции доступны по одному нажатию мыши. <br/>
+                        С легкостью настраивайте параметры, получайте обновления и обеспечивайте безопасность — все это
+                        в одном месте, без лишних усилий!
                     </p>
+
+                    <div className="about__btn">
+                        <div className="about__btn-title">
+                            <img src={upd} alt="update" className="about__btn-img"/>
+                            <span className="about__title">Windows UPDATE</span>
+                        </div>
+                        <input
+                            className="about__switch"
+                            type="checkbox"
+                            id="switch__update"
+                            checked={isUpdateChecked}
+                            onChange={handleUpdateToggle}
+                        />
+                        <label htmlFor="switch__update" className="switch__label">
+                            <span className="switch__circle"></span>
+                        </label>
+                    </div>
                 </div>
+
                 <div className="about__item item-2">
                     <div className="about__title-content">
                         <span className="about__title">БЭКАПЫ</span>
