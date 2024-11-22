eebeb72972f290c49a0c6fdb8cb26fbf1d5efaa8
bollshoy <b.orlov7709@gmail.com>
2024-11-22 13:01:37 +0200

    upd

--- src/components/Recleaner/Recleaner.jsx
+++ src/components/Recleaner/Recleaner.jsx
@@ -1,16 +1,20 @@
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 import recleaner from "@/data/recleaner.js";
 import logo from '@/assets/images/recleanerLogo.jpg';
 import './Recleaner.css';
 
 const Recleaner = () => {
-    const downloadUrl = import.meta.env.VITE_DOWNLOAD_LINK;
-    const hiddenLinkRef = useRef(null);
+    const buttonRef = useRef(null);
 
     const handleDownload = () => {
-        hiddenLinkRef.current.click();
+        const fileUrl = import.meta.env.VITE_DOWNLOAD_LINK;
+        const link = document.createElement('a');
+        link.href = fileUrl;
+        link.setAttribute('download', 'Recleaner.exe');
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
     };
-
     return (
         <section className="recleaner">
             <div className="recleaner__container container">
@@ -25,15 +29,9 @@ const Recleaner = () => {
                             С обширным функционалом, при этом не ломая систему.<br/>
                             Программа абсолютно бесплатная, скачать может любой желающий.
                         </p>
-                        <button className="recleaner__content--btn" onClick={handleDownload}>
-                            скачать
+                        <button className={'recleaner__content--btn'} onClick={handleDownload} ref={buttonRef}>
+                            Скачать
                         </button>
-                        <a
-                            href={downloadUrl}
-                            download="RECLEANER.exe"
-                            ref={hiddenLinkRef}
-                            style={{ display: 'none' }}>
-                        </a>
                     </div>
                     <div className="recleaner__content-img">
                         <img src={logo} alt="logo"/>
@@ -42,7 +40,8 @@ const Recleaner = () => {
                 <div className="recleaner__social">
                     {recleaner.map((item) => (
                         <div className="recleaner__item" key={item.id}>
-                            <a href={item.href} target={'_blank'} className="recleaner__item--link">
+                            <a href={item.href} target={'_blank'} rel="noopener noreferrer"
+                               className="recleaner__item--link">
                                 <img src={item.src} alt="" className="recleaner__item--img"/>
                             </a>
                         </div>
