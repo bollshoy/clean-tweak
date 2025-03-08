d68932de5831ce70cab8b565b5ecb5fc27e60f49
bollshoy <b.orlov7709@gmail.com>
2025-03-08 16:16:28 +0200

    fix vitecfg

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
-import ViteCompression from "vite-plugin-compress";
+const ViteCompression = require("vite-plugin-compress");
 import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
