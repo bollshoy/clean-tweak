4a2a216ee2aa68d682946f106e4431fdec5f0f88
bollshoy <b.orlov7709@gmail.com>
2025-03-08 17:23:19 +0200

    remove

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
-import ViteCompression from 'vite-plugin-compress';
-import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   optimizeDeps: {
@@ -12,31 +10,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    ViteCompression({
-      exclude: ['**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.otf'],
-    }),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 75,
-      },
-      pngquant: {
-        quality: [0.6, 0.8],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          { removeViewBox: false },
-          { cleanupIDs: false },
-        ],
-      },
-    }),
   ],
   base: "./",
   resolve: {
