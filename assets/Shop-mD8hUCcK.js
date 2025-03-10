a22ae391ddbc6ee20cc2eb4ad420ca63cb65389d
bollshoy <bollshoy@users.noreply.github.com>
2025-03-10 08:27:59 +0000

    deploy: 3f68651a50014fa0347f0b5ecb5a2346b13ca2bf

---
+++ assets/Shop-mD8hUCcK.js
@@ -0,0 +1,120 @@
+import{r as u,j as e,H as g}from"./index-CkaHJAYz.js";const x=""+new URL("banner-Dqb0QzsK.webp",import.meta.url).href,b=()=>{const i=u.useCallback(()=>{window.scrollBy({top:1e3,behavior:"smooth"})});return e.jsx("section",{className:"banner",children:e.jsxs("div",{className:"banner__container container",children:[e.jsxs("div",{className:"banner__content",children:[e.jsx("span",{className:"banner__subtitle",children:"shop"}),e.jsxs("h6",{className:"banner__title",children:["Магазин услуг ",e.jsx("br",{}),"CleanTweaking."]}),e.jsx("button",{onClick:i,className:"banner__link",children:"К покупкам"})]}),e.jsx("div",{className:"banner__img",children:e.jsx("img",{src:x,alt:"img"})})]})})},j=""+new URL("optimization-BVO14xz9.png",import.meta.url).href,w=""+new URL("coding-BNyFHgaE.png",import.meta.url).href,U=""+new URL("support-BI3Gnypk.png",import.meta.url).href,N=""+new URL("basic-CzAFe6rP.webp",import.meta.url).href,a=""+new URL("ultimate-CPsixcS3.webp",import.meta.url).href,R=""+new URL("categoriesSystemSettings-HUN1iToj.webp",import.meta.url).href,l=""+new URL("overclocking-CRoAATEx.webp",import.meta.url).href,m=""+new URL("discordBot-C8u6kPGY.webp",import.meta.url).href,d=""+new URL("telegramBot-9vqW8MlX.webp",import.meta.url).href,p=""+new URL("website-UthnF6sq.webp",import.meta.url).href,h=""+new URL("otherPrograming-B6uQiYtL.webp",import.meta.url).href,c=""+new URL("boostMM-Cd0kJJ7m.webp",import.meta.url).href,o=""+new URL("boostFaceIT-Dvd7oL2L.webp",import.meta.url).href,r=[{id:1,title:"Настройка",items:[{title:"Базовая оптимизация",src:N,description:"Базовая оптимизация – первый шаг к быстрой и стабильной системе! Настроим систему, параметры и реестр, очистим от лишнего – ваша ОС будет работать быстрее и эффективнее!",time:"2 часа"},{title:"Ultimate оптимизация",src:a,description:"Ultimate оптимизация – максимальная производительность вашей системы! Полная очистка, установка свежей ОС, тонкая настройка служб и реестра. Сделаем вашу систему стабильной, быстрой и готовой к любым нагрузкам!",time:"3-4 часа"},{title:"Настройка устройств",src:R,description:"Оптимизация интернета для повышения скорости, стабильности соединения, понижения PINGа в играх и понижения BufferBloat. Доверьтесь нам! Улучшите качество, настройте оборудование, выберите провайдера.",time:"30 минут"},{title:"Качественный разгон системы",src:l,description:"Профессиональный разгон процессора/видеокарты/ОЗУ для повышения производительности, увеличения FPS и улучшения игрового опыта. Гарантируем безопасную настройку и стабильную работу системы.",time:"2 часа"}],buyItem:[{title:"Базовая оптимизация",content:[`
+					1. Переустановка Windows с использованием официального образа
+
+					2. Активация Windows и установка программного обеспечения
+
+					3. Установка всех необходимых драйверов
+
+					4. Персональная настройка Windows
+
+					5. Настройка монитора и устройств
+
+				`],description:`
+					Повышается FPS, снижаются задержки, изображение
+					становится плавнее, а мышь реагирует быстрее. Работа выполняется
+					удаленно через AnyDesk, RuDesktop или RustDesk, также возможен выезд
+					в пределах МКАД (цена может варьироваться).
+				`,priceRUB:"2000 RUB",priceEU:"25 EU"},{title:"Ultimate оптимизация",src:a,content:[`
+					1. Переустановка & Полноценная Настройка Windows
+
+					2. Разгон + Понижение таймингов ОЗУ
+
+					3. Настройка Роутера, Интернета
+
+					4. Разгон / Андервольтинг CPU
+
+					5. Разгон / Андервольтинг GPU
+
+					6. Прошивка & Настройка Bios
+				`],description:`
+					Повышается FPS, снижаются задержки, изображение становится
+					плавнее, а мышь реагирует резче. Работа через AnyDesk, возможен
+					выезд в пределах МКАД (цена может варьироваться). Бесплатные
+					консультации по апгрейду включены.
+				`,priceRUB:"6500 RUB",priceEU:"75 EU"},{title:"Разгон системы",src:l,content:[`
+					1. Обеспечение оптимальной производительности
+
+					2. Индивидуальная настройка и поддержка 24/7
+
+					3. Оптимизация игрового процесса
+
+					4. Гарантия стабильности системы
+
+					5. Профессиональный подход
+				`],description:`
+					Разгон процессора и видеокарты — способ повысить
+					производительность вашего компьютера и улучшить качество игр.
+					Доверьтесь профессионалам для максимума из вашего оборудования!
+				`,priceRUB:"3500 RUB",priceEU:"40 EU"}],src:j},{id:2,title:"Разработка",items:[{title:"Разработка бота на Discord",src:m,description:"Разработка интерактивных ботов для Discord, которые улучшают взаимодействие в сообществах, автоматизируют процессы, помогают в модерации и создают удобные инструменты для участников.",time:"От сложности"},{title:"Разработка бота на Telegram",src:d,description:"Разработка ботов для Telegram с расширенными возможностями автоматизации, которые упрощают выполнение задач, повышают эффективность и экономят время.",time:"От сложности"},{title:"Разработка сайтов",src:p,description:"Разработка современных и адаптивных сайтов, полностью соответствующих требованиям бизнеса и пользователей. Обеспечиваем удобство, скорость и функциональность для эффективного онлайн-присутствия.",time:"От сложности"},{title:"Прочие разработки",src:h,description:"Разработка индивидуальных решений для различных программных задач и проектов на C# и Python, с учетом всех требований для обеспечения надежности, функциональности и эффективности.",time:"От сложности"}],buyItem:[{title:"Разработка бота на Discord",src:m,content:`
+					1. Создание удобных инструментов для участников.
+					2. Улучшение взаимодействия в сообществах.
+					3. Автоматизация процессов.
+					4. Помощь в модерации.
+				`,description:`
+				Создание интерактивных ботов для платформы
+				Discord, которые способствуют улучшению
+				взаимодействия между участниками сообществ,
+				автоматизируют различные процессы, помогают в
+				эффективной модерации и предоставляют удобные
+				инструменты для пользователей, значительно
+				повышая качество общения и взаимодействия.
+				`},{title:"Разработка бота на Telegram",src:d,content:`
+					1. Расширенные возможности автоматизации.
+					2. Упрощение выполнения задач.
+					3. Повышение эффективности.
+					4. Экономия времени.
+				`,description:`
+				Создание ботов для мессенджера Telegram с улучшенными
+				функциями автоматизации, которые упрощают выполнение
+				задач, повышают эффективность работы и экономят время
+				пользователей, делая взаимодействие более удобным и
+				продуктивным.
+				`},{title:"Разработка сайтов",src:p,content:`
+					1. Соответствие требованиям бизнеса и пользователей.
+					2. Разработка современных и адаптивных сайтов.
+					3. Эффективное онлайн-присутствие.
+					4. Обеспечение удобства и скорости.
+				`,description:`
+				Создание современных и адаптивных веб-сайтов, которые
+				отвечают потребностям бизнеса и ожиданиям пользователей.
+				Мы гарантируем высокое удобство, быструю загрузку и
+				функциональность для успешного онлайн-присутствия.
+				`},{title:"Прочие разработки",src:h,content:`
+					1. Обеспечение надежности, функциональности и эффективности.
+					2. Решение различных программных задач и проектов.
+					3. Разработка индивидуальных решений.
+					4. Использование языков C# и Python.
+				`,description:`
+				Создание персонализированных решений для разнообразных программных
+				задач и проектов с использованием языков C# и Python, учитывая все
+				требования для достижения высокой надежности, функциональности и
+				общей эффективности.
+				`}],src:w},{id:3,title:"Прочее",items:[{title:"Буст звание в ММ с бустером",src:c,description:"Повышение звания в Matchmaking с помощью профессионального бустера, гарантируя быстрый и безопасный рост без потери доступа к аккаунту.",time:"От начального lvl"},{title:"Буст звание в ММ лично",src:c,description:"Личное повышение звания в Matchmaking с индивидуальным подходом, обеспечивая качественный результат без рисков для аккаунта.",time:"От начального звание"},{title:"Буст звание на FaceIT",src:o,description:"Повышение звания на FaceIT с помощью опытных игроков, обеспечивая стабильный рост и качественный результат.",time:"От начального lvl"},{title:"Верификация на FaceIT",src:o,description:"Содействие в прохождении верификации аккаунта на FaceIT, обеспечивая быстрое и корректное выполнение всех требований платформы.",time:"15-30 минут"}],buyItem:[{title:"Буст звание в ММ с бустером",src:c,content:`
+					1. Использование профессионального бустера.
+					2. Гарантия быстрого и безопасного роста.
+					3. Повышение звания в Matchmaking.
+					4. Сохранение доступа к аккаунту.
+				`,description:`
+					Увеличение звания в Matchmaking с помощью опытного
+					бустера, обеспечивая быстрый и безопасный прогресс
+					без риска потери доступа к вашему аккаунту.
+				`},{title:"Буст звание в ММ лично",src:c,content:`
+					1. Индивидуальный подход к каждому клиенту.
+					2. Обеспечение качественного результата.
+					3. Личное повышение звания в Matchmaking.
+					4. Отсутствие рисков для аккаунта.
+				`,description:`
+					Индивидуальное повышение звания в Matchmaking с
+					персонализированным подходом, гарантируя
+					высококачественный результат без каких-либо рисков
+					для безопасности вашего аккаунта.
+				`},{title:"Буст звание на FaceIT",src:o,content:`
+					1. Гарантия качественного результата.
+					2. Использование опытных игроков.
+					3. Обеспечение стабильного роста.
+					4. Повышение ElO на FaceIT.
+				`,description:`
+					Увеличение звания на FaceIT с помощью профессиональных
+					игроков, что обеспечивает стабильный прогресс и
+					высококачественный результат в игре.
+				`}],src:U}],k=({selectedCategory:i,handleCategoryChange:s})=>(r.find(t=>t.title===i),e.jsx("section",{className:"categories",children:e.jsx("div",{className:"categories__container",children:e.jsxs("div",{className:"categories__content",children:[e.jsx("span",{className:"categories__title",children:"Категории"}),e.jsx("div",{className:"categories__category",children:r.map(t=>e.jsx("div",{className:`category__item ${i===t.title?"active":""}`,onClick:()=>s(t.title),children:e.jsxs("button",{className:"category__btn",children:[e.jsx("img",{src:t.src,alt:t.title,className:"category__icon"}),t.title]})},t.id))})]})})})),f=({selectedCategory:i})=>{var t;const s=r.find(n=>n.title===i);return e.jsx("section",{className:"product",children:e.jsx("div",{className:"product__container",children:e.jsxs("div",{className:"categories__list",children:[e.jsxs("h6",{className:"product__title",children:["Категория ",i,":"]}),e.jsx("ul",{className:"product__list",children:(t=s==null?void 0:s.items)!=null&&t.length?s.items.map((n,_)=>e.jsx("li",{className:"product__item",style:{backgroundImage:`url(${n.src})`},children:e.jsxs("div",{className:"product__overlay",children:[e.jsx("span",{className:"product__item-title",children:n.title}),e.jsx("div",{className:"product__item-line"}),e.jsx("p",{className:"product__item-text",children:n.description}),e.jsxs("p",{className:"product__item-time",children:["Время выполнения услуги: ",e.jsx("span",{children:n.time})]}),e.jsx("a",{target:"_blank",href:"https://discord.gg/Ea8FuHNuJT",className:"product__item-link",children:"Купить"})]})},_)):e.jsx("p",{children:"Нет доступных элементов"})})]})})})},B=()=>{const[i,s]=u.useState(r[0].title),t=n=>{s(n)};return e.jsxs("section",{className:"shop",id:"shop",children:[e.jsx(g,{}),e.jsx(b,{}),e.jsx(k,{selectedCategory:i,handleCategoryChange:t}),e.jsx(f,{selectedCategory:i})]})};export{B as default};
--- assets/download-m2k7JacB.js
+++
@@ -1,1 +0,0 @@
-const o=""+new URL("download-QsznVm9R.png",import.meta.url).href;export{o as d};
